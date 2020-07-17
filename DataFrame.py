1e121597bd1d1cac571e8ee6faafecbefb7928d3
Rafi <rafael.cohngruenwald@gmail.com>
2020-07-17 14:59:31 -0700

    filtering out nones

--- DataFrame.py
+++ DataFrame.py
@@ -19,7 +19,8 @@ class TweetsDataFrame:
             Returns the tweets dataframe
         '''
         rdd = sc.textFile('data/french_tweets.json')
-        filtered_rdd = rdd.map(self.convert_to_dict).map(self.parse_tweet)
+        filtered_rdd = rdd.map(self.convert_to_dict).filter(lambda x: False if x == None else True).map(self.parse_tweet).filter(lambda x: False if x == None else True)
+        
         
         schema = StructType([
             StructField('user_id', StringType(), True),
@@ -51,12 +52,20 @@ class TweetsDataFrame:
             return None
         
     def parse_tweet(self, tweet_dict):
+        
+        if 'created_at' not in tweet_dict.keys():
+            return None
         create_date = datetime.datetime.strptime(tweet_dict['created_at'], '%a %b %d %H:%M:%S %z %Y')
+        
         text = tweet_dict['text']
         user_id = int(tweet_dict['user']['id'])
         followers = int(tweet_dict['user']['followers_count'])
         verified = bool(tweet_dict['user']['verified'])
+        
+        if (tweet_dict['place'] is None) or ('place_type' not in tweet_dict['place'].keys()):
+            return None
         place_type = tweet_dict['place']['place_type']
+        
         place_name = tweet_dict['place']['name']
         place_full_name = tweet_dict['place']['full_name']
         country_code = tweet_dict['place']['country_code']
@@ -64,7 +73,7 @@ class TweetsDataFrame:
         retweets = int(tweet_dict['retweet_count'])
         favorites = int(tweet_dict['favorite_count'])
         lang = tweet_dict['lang']
-
+        
         return (user_id, followers, verified, text,
                 create_date.year, create_date.month,
                 create_date.day, create_date.hour,
